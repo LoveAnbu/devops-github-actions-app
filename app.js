aa95bd8f68f74b0adc708f17fc76208523d1ff04
Ubuntu <ubuntu@ip-10-19-0-42.ec2.internal>
2026-05-02 06:29:41 +0000

    initial github actions setup

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  console.log("Request received");
+  res.send("Hello from GitHub Actions 🚀");
+});
+
+app.listen(3000, () => {
+  console.log("App running on port 3000");
+});
